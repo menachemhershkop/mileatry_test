6249a2868fa3fe245569a99bed98ef3058a2093a
Menachem Hershkop <Hers.mm@gmail.com>
2026-03-16 11:00:05 +0200

    delete By id

--- server/rauots/apiRoute.js
+++ server/rauots/apiRoute.js
@@ -4,33 +4,40 @@ import run from '../db/connect.js';
 
 export const apiRoute = express();
 const db = run()
-apiRoute.get('/launchers',async (req, res)=>{
-    res.send(await db.then(data=> data.find({}).toArray()))
+apiRoute.get('/launchers', async (req, res) => {
+    res.send(await db.then(data => data.find({}).toArray()))
 })
-apiRoute.post('/launchers',async (req, res)=>{
-    console.log(req.body);
-    
-    const {id,city,rocketType,latitude,longitude,name} = req.body
-    if (id,!city,!rocketType,!latitude,!longitude,!name){
-        res.status(401).json({msg: 'Requyemnts felds less'})
-    }
-    else{
-        const resuilt  = await db.then(data=> data.insertOne({id:id,city:city,rocketType:rocketType,latitude:latitude,longitude:longitude,name:name}))
-        res.status(201).json({msg:resuilt.insertedId})
-    }
-})
-apiRoute.get('/launchers/:id',async (req, res)=>{
-    const param = req.params.id
+apiRoute.post('/launchers', async (req, res) => {
 
-    
-    const finder = await db.then(data=> data.find({id:param}).toArray())
-    if (finder.length === 0){
-        res.status(404).json({msg:'id not found'})
+    const { id, city, rocketType, latitude, longitude, name } = req.body
+    if (id, !city, !rocketType, !latitude, !longitude, !name) {
+        res.status(401).json({ msg: 'Requyemnts felds less' })
     }
-    else{
-        res.status(200).json({masger:finder})
+    else {
+        const resuilt = await db.then(data => data.insertOne({ id: id, city: city, rocketType: rocketType, latitude: latitude, longitude: longitude, name: name }))
+        res.status(201).json({ msg: resuilt.insertedId })
     }
 })
-apiRoute.delete('/launchers:id', (req, res)=>{
-    
+apiRoute.get('/launchers/:id', async (req, res) => {
+    const param = req.params.id
+    const finder = await db.then(data => data.find({ id: param }).toArray())
+    if (finder.length === 0) {
+        res.status(404).json({ msg: 'id not found' })
+    }
+    else {
+        res.status(200).json({ masger: finder })
+    }
+})
+apiRoute.delete('/launchers/:id', async (req, res) => {
+const param = req.params.id
+console.log(param);
+
+    const finder = await db.then(data => data.find({ id: param }).toArray())
+    if (finder.length === 0) {
+        res.status(404).json({ msg: 'id not found' })
+    }
+    else {
+        const del = await db.then(data => data.deleteOne({ id: param }))
+        res.status(200).json({ msg: del.deletedCount})
+    }
 })
