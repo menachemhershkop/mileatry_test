5831c6cb96855928f73d6073e1d87874aa3968f2
Menachem Hershkop <Hers.mm@gmail.com>
2026-03-16 10:42:30 +0200

    get and post to DB

--- server/db/connect.js
+++ server/db/connect.js
@@ -5,13 +5,16 @@ const client = new MongoClient(process.env.MONGO_CONNECT);
 
 export default async function run() {
   try {
-    await client.connect();
+   await client.connect();
     console.log('Connected to MongoDB');
-    return client.db('miltery')
+    return client.db('miltery').collection('racet')
 
     
 } catch (error) {
     console.error('Connection error:', error);
 } 
 }
+
+// console.log(await run().then(data=> data.find({}).toArray()))
+
 
--- server/rauots/apiRoute.js
+++ server/rauots/apiRoute.js
@@ -1,4 +1,28 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import run from '../db/connect.js';
 
 export const apiRoute = express();
+const db = run()
+apiRoute.get('/launchers',async (req, res)=>{
+    res.send(await db.then(data=> data.find({}).toArray()))
+})
+apiRoute.post('/launchers',async (req, res)=>{
+    console.log(req.body);
+    
+    const {id,city,rocketType,latitude,longitude,name} = req.body
+    if (id,!city,!rocketType,!latitude,!longitude,!name){
+        res.status(401).json({msg: 'Requyemnts felds less'})
+    }
+    else{
+        const resuilt  = await db.then(data=> data.insertOne({id:id,city:city,rocketType:rocketType,latitude:latitude,longitude:longitude,name:name}))
+        console.log(resuilt.insertedId);
+        
+    }
+})
+apiRoute.get('/launchers/:id', (req, res)=>{
+    
+})
+apiRoute.delete('/launchers:id', (req, res)=>{
+    
+})
